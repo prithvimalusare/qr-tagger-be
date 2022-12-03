b051c4493ff8cbc19407bc93571b74db62d9846f
personal_github <malusareprithvi@gmail.com>
2022-12-03 13:47:20 +0530

    index route inserted

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const {authControllers} = require('../controllers');
 const {registerValidaiton} = require('../helper/validationMiddleware')
 
-router.post('/register',registerValidaiton,authControllers.register)
+router.post('/register', registerValidaiton, authControllers.register)
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/',  (req,res,next)=>{
+    res.status(200).send('Server is working')
+} )
+
 router.use('/auth', require('./auth'));
 
 router.all('*', (req,res,next)=>{
